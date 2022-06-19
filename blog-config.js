1b190fd1a78404fe47ab9a5dc86bfb338115d973
ByungKookKoo <byunguki@kyonggi.ac.kr>
2022-06-20 06:01:55 +0900

    blog

---
+++ blog-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: "bkkoo.dev",
+  description: "안녕하세요!",
+  author: "bkkoo",
+  siteUrl: "https://byungkookkoo.github.io/",
+  links: {
+    github: "https://github.com/byungkookkoo",
+    linkedIn: "https://www.linkedin.com/in/byungkook-koo/",
+    facebook: "https://www.facebook.com",
+    email: "mailto:gukman97@gmail.com",
+  },
+  utterances: {
+    repo: "byungkookkoo/comments",
+    type: "pathname",
+  },
+}
